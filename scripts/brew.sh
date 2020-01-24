6ba3d36fdb49a69ad4a1d50bea91ba029654fa9a
Marc Miller <marc.miller.4@us.af.mil>
2020-01-24 15:28:09 -0500

    updated brew script to install fonts differently on MacOS as required

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -5,7 +5,7 @@
 # ===============================================================
 
 printf "Updating Homebrew... " 
-brew update &
+brew update > /dev/null 2>&1 &
 wait
 printf "done! \n"
 
@@ -14,7 +14,7 @@ printf "done! \n"
 # ===============================================================
 
 printf "Upgrading packages... "
-#brew upgrade &
+#brew upgrade > /dev/null 2>&1 &
 #wait
 printf "done! \n"
 
@@ -26,9 +26,9 @@ printf "done! \n"
 printf "Installing $PLATFORM helpers for Homebrew... "
 
 case $PLATFORM in
-    "MacOS") brew install coreutils
+    "MacOS") brew install coreutils > /dev/null 2>&1 &
     ;;
-    "Linux") sudo apt install build-essential curl file git
+    "Linux") sudo apt install build-essential curl file git > /dev/null 2>&1 &
     ;;
     *) printf "Unable to find helpers for Homebrew for $PLATFORM. \n"
     ;;
@@ -41,9 +41,17 @@ printf "done! \n"
 # ===============================================================
 
 printf "Installing Nerdfonts Hasklug... "
-brew tap homebrew/cask-fonts &
+brew tap homebrew/cask-fonts > /dev/null 2>&1 &
 wait
-brew cask install font-hasklig-nerd-font-mono &
+
+case $PLATFORM in
+    "MacOS") brew cask install --fontdir=/Library/Fonts font-hasklig-nerd-font-mono > /dev/null 2>&1 &
+    ;;
+    "Linux") brew cask install font-hasklig-nerd-font-mono > /dev/null 2>&1 &
+    ;;
+    *) printf "Unable to find helpers for Homebrew for $PLATFORM. \n"
+    ;;
+esac
 wait
 printf "done! \n"
 
--- scripts/zsh_setup.sh
+++ scripts/zsh_setup.sh
@@ -14,7 +14,7 @@ fi
 printf "Checking for Oh-My-ZSH... "
 if [[ ! -d '$HOME/.oh-my-zsh' ]]; then
     printf "Not found, installing... "
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null &
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1 &
     wait
     printf "done! \n"
     printf "Replacing default Oh-My-ZSH template .zshrc with ours ..."
@@ -28,7 +28,7 @@ fi
 printf "Checking for Powerlevel10K... "
 if [[ ! -d '$HOME/.oh-my-zsh/custom/themes/powerlevel10k' ]]; then
     printf "Adding Powerlevel10K... "
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k > /dev/null &
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k > /dev/null 2>&1 &
     wait
     printf "done! \n"
 else
